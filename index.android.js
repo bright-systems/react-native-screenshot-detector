c6bc9eccb7af54960cb94cbb2ad1b720e7d25ec5
Mark Vayngrib <mark@tradle.io>
2019-07-04 09:49:19 -0400

    add enableScreenshots, disableScreenshots on android

--- index.android.js
+++ index.android.js
@@ -1,8 +1,13 @@
 // TODO: check out https://github.com/abangfadli/shotwatch
 
+import { NativeModules } from 'react-native'
+
 const unsubscribe = () => {}
 const subscribe = () => unsubscribe
 
-export default {
+const Detector = {
+  ...NativeModules.RNScreenshotDetector,
   subscribe,
 }
+
+export default Detector
